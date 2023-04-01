157f507228c2e61b97962c7611bcb91c48175447
Ewerton49592 <100850927+Ewerton49592@users.noreply.github.com>
2023-04-01 11:22:07 -0300

    Update instalar-chrome.sh

--- instalar-chrome.sh
+++ instalar-chrome.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
 
-# Adiciona a chave de assinatura do Brave ao sistema
-curl https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
+# Faz o download do Firefox
+wget -O firefox.tar.bz2 "https://download.mozilla.org/?product=firefox-latest&os=linux64&lang=en-US"
 
-# Adiciona o repositório do Brave ao sources.list
-echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+# Extrai o arquivo baixado
+tar xjf firefox.tar.bz2 -C /opt/
 
-# Atualiza a lista de pacotes disponíveis
-sudo apt update
+# Cria um link simbólico para o Firefox
+sudo ln -s /opt/firefox/firefox /usr/bin/firefox
 
-# Instala o Brave
-sudo apt install brave-browser
-
-# Define o Brave como o navegador padrão
-sudo update-alternatives --set x-www-browser /usr/bin/brave-browser
-sudo update-alternatives --set gnome-www-browser /usr/bin/brave-browser
-
+# Define o Firefox como o navegador padrão
+sudo update-alternatives --set x-www-browser /usr/bin/firefox
+sudo update-alternatives --set gnome-www-browser /usr/bin/firefox
