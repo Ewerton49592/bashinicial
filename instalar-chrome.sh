7171d8ece87d72cc2d6e10fc2b59dbeadca0abd2
Ewerton49592 <100850927+Ewerton49592@users.noreply.github.com>
2023-04-01 11:19:14 -0300

    Update instalar-chrome.sh

--- instalar-chrome.sh
+++ instalar-chrome.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
-# Adiciona a chave de assinatura do Google Chrome ao sistema
-wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+# Adiciona a chave de assinatura do Brave ao sistema
+curl https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
 
-# Adiciona o repositório do Google Chrome ao sources.list
-echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" | sudo tee /etc/apt/sources.list.d/google-chrome.list
+# Adiciona o repositório do Brave ao sources.list
+echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 
 # Atualiza a lista de pacotes disponíveis
 sudo apt update
 
-# Instala o Google Chrome
-sudo apt install google-chrome-stable
+# Instala o Brave
+sudo apt install brave-browser
+
+# Define o Brave como o navegador padrão
+sudo update-alternatives --set x-www-browser /usr/bin/brave-browser
+sudo update-alternatives --set gnome-www-browser /usr/bin/brave-browser
+
