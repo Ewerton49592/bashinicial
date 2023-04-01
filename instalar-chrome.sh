e40cc500944f500bdca546c6ecfa44a8866cb373
Ewerton49592 <100850927+Ewerton49592@users.noreply.github.com>
2023-03-31 21:19:44 -0300

    Update instalar-chrome.sh

--- instalar-chrome.sh
+++ instalar-chrome.sh
@@ -1,22 +1,37 @@
 #!/bin/bash
 
-# Verifica se o sistema é Ubuntu
-if [ $(lsb_release -is) != "Ubuntu" ]; then
-    echo "Este script é compatível apenas com sistemas Ubuntu."
-    exit 1
-fi
+# Atualiza e faz o upgrade do sistema
+sudo apt update && sudo apt upgrade -y
 
-# Baixa o pacote de instalação do Google Chrome
+# Instala o ambiente gráfico xfce4
+sudo apt install xfce4 -y
+
+# Instala o Google Chrome
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-
-# Instala o pacote de instalação do Google Chrome
 sudo dpkg -i google-chrome-stable_current_amd64.deb
-
-# Instala as dependências do Google Chrome
-sudo apt-get -f install -y
-
-# Remove o pacote de instalação do Google Chrome
+sudo apt-get install -f -y
 rm google-chrome-stable_current_amd64.deb
 
-# Exibe uma mensagem indicando que a instalação foi concluída
-echo "A instalação do Google Chrome foi concluída com sucesso."
+# Instala o zip e o unzip
+sudo apt install zip unzip -y
+
+# Instala o rclone
+curl https://rclone.org/install.sh | sudo bash
+
+# Instala o pip
+sudo apt install python3-pip -y
+
+# Seta permissão 777 para a pasta /mnt
+sudo chmod 777 /mnt
+
+# Instala dependências para compilar o chia-plotter
+sudo apt install -y libsodium-dev cmake g++ git build-essential
+
+# Baixa o chia-plotter e compila
+git clone https://github.com/madMAx43v3r/chia-plotter.git
+cd chia-plotter
+git submodule update --init
+./make_devel.sh
+
+# Adiciona o chia-plotter como global
+sudo ln -s /path/to/chia-plotter/build/chia_plot /usr/local/bin/chia_plot
