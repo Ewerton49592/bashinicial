0713e499fcd197c190e816243cf9331727582c5e
Ewerton49592 <100850927+Ewerton49592@users.noreply.github.com>
2023-04-01 11:12:00 -0300

    Update instalar-chrome.sh

--- instalar-chrome.sh
+++ instalar-chrome.sh
@@ -1,15 +1,13 @@
-
 #!/bin/bash
 
-# Atualiza a lista de pacotes e instala o xfce4-goodies
+# Adiciona a chave de assinatura do Google Chrome ao sistema
+wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+
+# Adiciona o repositório do Google Chrome ao sources.list
+echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" | sudo tee /etc/apt/sources.list.d/google-chrome.list
+
+# Atualiza a lista de pacotes disponíveis
 sudo apt update
-sudo apt install xfce4-goodies -y
 
-# Cria um arquivo de inicialização do Xfce4
-echo "xfce4-session" > ~/.xsession
-
-# Configura o gerenciador de exibição LightDM para usar o Xfce4
-sudo sed -i 's/^user-session=.*/user-session=xfce4/' /etc/lightdm/lightdm.conf
-
-# Reinicia o sistema para que as mudanças entrem em vigor
-sudo reboot
+# Instala o Google Chrome
+sudo apt install google-chrome-stable
