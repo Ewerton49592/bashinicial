02ffde303b215ba6a8deea3c6001a872b70626a8
Ewerton49592 <100850927+Ewerton49592@users.noreply.github.com>
2023-04-02 12:47:23 -0300

    Update instala.sh

--- instala.sh
+++ instala.sh
@@ -1,22 +1,13 @@
 #!/bin/bash
 
+# Faz o download da chave pública do Google e a adiciona ao sistema
+wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+
+# Adiciona o repositório do Google Chrome ao sistema
+echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" | sudo tee /etc/apt/sources.list.d/google-chrome.list
+
 # Atualiza a lista de pacotes do sistema
 sudo apt-get update
 
-# Executa a atualização do sistema
-sudo apt-get upgrade --assume-yes
-
-# Instala o pacote xfce4-goodies
-sudo apt-get install --assume-yes xfce4-goodies
-
-# Baixa o pacote .deb do Google Chrome
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -P /tmp
-
-# Instala o Google Chrome a partir do pacote .deb baixado
-sudo dpkg -i /tmp/google-chrome-stable_current_amd64.deb
-
-# Instala as dependências que podem ter ficado faltando
-sudo apt-get -f install --assume-yes
-
-# Remove o pacote .deb baixado
-rm /tmp/google-chrome-stable_current_amd64.deb
+# Instala o Google Chrome
+sudo apt-get install -y google-chrome-stable
