b310ffd6d0842ed53fbe0f63952edfa40f7c5ad0
Ewerton49592 <100850927+Ewerton49592@users.noreply.github.com>
2023-04-02 11:14:24 -0300

    Update instala.sh

--- instala.sh
+++ instala.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-sudo apt update
-sudo apt upgrade -y
-sudo apt install xfce4-goodies -y
 
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo dpkg -i google-chrome-stable_current_amd64.deb
-sudo apt-get install -f
+# Configura o painel do XFCE
+xfce4-panel --quit && rm -rf ~/.config/xfce4/panel && xfce4-panel
