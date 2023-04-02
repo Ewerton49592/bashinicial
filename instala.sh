dfaed3f60cba23c4c40769652b3a86dfbaf369f6
Ewerton49592 <100850927+Ewerton49592@users.noreply.github.com>
2023-04-02 12:18:58 -0300

    Update instala.sh

--- instala.sh
+++ instala.sh
@@ -3,11 +3,20 @@
 # Atualiza a lista de pacotes do sistema
 sudo apt-get update
 
-# Instala o ambiente gráfico XFCE4 e outras dependências necessárias
-sudo apt-get install --assume-yes xfce4 desktop-base dbus-x11 xscreensaver
+# Executa a atualização do sistema
+sudo apt-get upgrade --assume-yes
 
-# Configura o gerenciador de exibição para iniciar o XFCE4 como ambiente gráfico padrão
-sudo bash -c 'echo "/usr/bin/startxfce4" > /etc/X11/xinit/xserverrc'
+# Instala o pacote xfce4-goodies
+sudo apt-get install --assume-yes xfce4-goodies
 
-# Reinicia o sistema para que as alterações tenham efeito
-sudo reboot
+# Baixa o pacote .deb do Google Chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -P /tmp
+
+# Instala o Google Chrome a partir do pacote .deb baixado
+sudo dpkg -i /tmp/google-chrome-stable_current_amd64.deb
+
+# Instala as dependências que podem ter ficado faltando
+sudo apt-get -f install --assume-yes
+
+# Remove o pacote .deb baixado
+rm /tmp/google-chrome-stable_current_amd64.deb
